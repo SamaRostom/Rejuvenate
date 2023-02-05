195539aa3311d8058fa0e5ee019be0e4f43c3853
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2023-02-05 13:02:44 +0200

    Create finalresult.dart

---
+++ rejuvenate_mobile_app/lib/screens/finalresult.dart
@@ -0,0 +1,51 @@
+// // ignore: import_of_legacy_library_into_null_safe
+// // import 'package:before_after/before_after.dart';
+// import 'package:flutter/material.dart';
+
+// class FinalResult extends StatefulWidget {
+//   static const routeName = "/final-result";
+//   const FinalResult({super.key});
+//   @override
+//   State<FinalResult> createState() => _FinalResultState();
+// }
+
+// class _FinalResultState extends State<FinalResult> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: SafeArea(
+//       child: Center(
+//       child: Column(
+//         // mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           const SizedBox(
+//             height: 50,
+//           ),
+
+//           Container(
+//             margin: const EdgeInsets.all(10.0),
+//             decoration: BoxDecoration(
+//               border: Border.all(color: Colors.white),
+//             ),
+//             child: BeforeAfter(
+//               beforeImage:Image.asset('assets/1.jpg'),
+//               afterImage: Image.asset('assets/2.jpg'),
+//               thumbColor: Colors.yellow,
+//               thumbRadius: 15.0,
+//               overlayColor: Colors.grey,
+//               isVertical: true,
+//             ),
+//           ),
+          
+//           // Center(
+//           //   child: RaisedButton(context)
+//           // )
+//         ],
+//       ),
+//       ),
+// ),
+      
+//     );
+//   }
+// }
