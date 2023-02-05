02899e67a7b47078f60cfca8073e7af40c347331
SamaRostom <sama1904157@miuegypt.edu.eg>
2023-02-05 21:01:20 +0200

    Create validations.dart

---
+++ rejuvenate_mobile_app/lib/utils/validations.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class CommonStyle {
+  static InputDecoration textFieldStyle(
+      {String labelText = "", String hintText = "", String? value}) {
+    return InputDecoration(
+      contentPadding: const EdgeInsets.all(12),
+      labelText: labelText,
+      hintText: hintText,
+      labelStyle: const TextStyle(color: Colors.grey),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: const BorderSide(color: Color.fromARGB(246, 86, 203, 209)),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: const BorderSide(color: Color.fromARGB(246, 86, 203, 209)),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      // fillColor: Colors.grey[200],
+      filled: true,
+    );
+  }
+}
+
+extension ExtString on String {
+  bool get isValidEmail {
+    final emailRegExp = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegExp.hasMatch(this);
+  }
+
+  bool get isValidName {
+    final nameRegExp =
+        RegExp(r"^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$");
+    return nameRegExp.hasMatch(this);
+  }
+
+  bool get isValidPassword {
+    final passwordRegExp = RegExp(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$");
+    return passwordRegExp.hasMatch(this);
+  }
+
+  bool isNull(String value) {
+    if (value.isEmpty) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  bool get isValidPhone {
+    final phoneRegExp = RegExp(r"^\+?0[0-9]{10}$");
+    return phoneRegExp.hasMatch(this);
+  }
+
+  bool get isValidAddress {
+    final phoneRegExp = RegExp(
+        r"^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z ]+[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z0-9-_ ]*$");
+
+    return phoneRegExp.hasMatch(this);
+  }
+
+  bool get isValidCode {
+    final phoneRegExp = RegExp(r"^[A-Z0-9]*$");
+    return phoneRegExp.hasMatch(this);
+  }
+
+//putting* allow Null !!
+//+ one or more
+  bool get isValidNumber {
+    final phoneRegExp = RegExp(r"^[0-9]+$");
+    return phoneRegExp.hasMatch(this);
+  }
+}
