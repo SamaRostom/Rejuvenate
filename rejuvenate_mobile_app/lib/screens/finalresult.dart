87dc49e46933fd12cc2e6bb5955d27cd5e0a1249
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2023-02-06 01:36:54 +0200

    Update finalresult.dart

--- rejuvenate_mobile_app/lib/screens/finalresult.dart
+++ rejuvenate_mobile_app/lib/screens/finalresult.dart
@@ -1,51 +1,49 @@
-// // ignore: import_of_legacy_library_into_null_safe
-// // import 'package:before_after/before_after.dart';
-// import 'package:flutter/material.dart';
+import 'package:before_after_image_slider_nullsafty/before_after_image_slider_nullsafty.dart';
+import 'package:flutter/material.dart';
 
-// class FinalResult extends StatefulWidget {
-//   static const routeName = "/final-result";
-//   const FinalResult({super.key});
-//   @override
-//   State<FinalResult> createState() => _FinalResultState();
-// }
+class FinalResult extends StatefulWidget {
+  const FinalResult({super.key});
+  @override
+  State<FinalResult> createState() => _FinalResultState();
+}
 
-// class _FinalResultState extends State<FinalResult> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       body: SafeArea(
-//       child: Center(
-//       child: Column(
-//         // mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           const SizedBox(
-//             height: 50,
-//           ),
+class _FinalResultState extends State<FinalResult> {
+  @override
+  Widget build(BuildContext context) {
+      // padding: const EdgeInsets.symmetric(horizontal: 24),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+      child: Center(
+      child: Column(
+        // height: 24.0,
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
 
-//           Container(
-//             margin: const EdgeInsets.all(10.0),
-//             decoration: BoxDecoration(
-//               border: Border.all(color: Colors.white),
-//             ),
-//             child: BeforeAfter(
-//               beforeImage:Image.asset('assets/1.jpg'),
-//               afterImage: Image.asset('assets/2.jpg'),
-//               thumbColor: Colors.yellow,
-//               thumbRadius: 15.0,
-//               overlayColor: Colors.grey,
-//               isVertical: true,
-//             ),
-//           ),
-          
-//           // Center(
-//           //   child: RaisedButton(context)
-//           // )
-//         ],
-//       ),
-//       ),
-// ),
-      
-//     );
-//   }
-// }
+          Container(
+            height: 600.0,
+            width: 450.0,
+            margin: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+            ),
+          child: BeforeAfter(
+              isVertical: true,
+              beforeImage: Image.asset('assets/Final1Test.jpg'),
+              afterImage: Image.asset('assets/Final2Test.jpg'),
+              thumbColor: Colors.cyan,
+              thumbRadius: 15.0,
+              overlayColor: Colors.grey,
+            ),
+          ),
+
+        ],
+        
+      ),),),
+    );
+  }
+}
