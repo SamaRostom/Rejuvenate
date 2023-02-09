0584de33c41e4c3fb3098be173f312f1343c2798
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2023-02-09 22:43:49 +0200

    Update main.dart

--- rejuvenate_mobile_app/lib/main.dart
+++ rejuvenate_mobile_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rejuvenate_mobile_app/screens/home.dart';
 
 import 'screens/editprofile.dart';
 import 'screens/finalresult.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
       ),
-      initialRoute: '/signup',
+      initialRoute: '/',
       routes: {
         '/login': (context) => const LoginScreen(),
         '/signup': (context) => const SignupScreen(),
@@ -42,6 +43,7 @@ class MyApp extends StatelessWidget {
         '/patientreport': (context) => const PatientReport(),
         '/uploadimage': (context) => const UploadImage(),
         '/savingprofileupdates': (context) => const SavingProfileUpdates(),
+        '/': (context) => const HomeScreen(),
       },
     );
   }
