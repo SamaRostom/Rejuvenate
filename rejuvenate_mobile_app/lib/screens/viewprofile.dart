addda209e0d323bd60e56ce099797e457d328e5c
SalmaSalahh <salma1905780@miuegypt.edu.eg>
2023-03-01 21:11:32 +0200

    Update viewprofile.dart

--- rejuvenate_mobile_app/lib/screens/viewprofile.dart
+++ rejuvenate_mobile_app/lib/screens/viewprofile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../utils/constants.dart';
+import '../utils/side_menu.dart';
 
 class ViewProfile extends StatefulWidget {
   const ViewProfile({super.key});
@@ -10,63 +12,69 @@ class _ViewProfileState extends State<ViewProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: loggedin == true ? const SideMenu1() : const SideMenu2(),
+      appBar: AppBar(
+        backgroundColor: Colors.cyan,
+      ),
       backgroundColor: Colors.white,
       body: SafeArea(
-      child: Center(
-      child: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 50,
-          ),
-          Stack(
+        child: Center(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
-                SizedBox(
-                width:150,
-                height:150,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(100),
-                  child: Image.asset('assets/viewprofileTest.jpg',fit: BoxFit.cover),
-                )),
-              
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: Container(
-                width: 35,
-                height: 35,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(100), color:Colors.cyan),
-                child: const Icon(Icons.edit,
-                color: Colors.white,
-                size:20,
-                ),
+              const SizedBox(
+                height: 50,
               ),
+              Stack(
+                children: [
+                  SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset('assets/viewprofileTest.jpg',
+                            fit: BoxFit.cover),
+                      )),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      width: 35,
+                      height: 35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.cyan),
+                      child: const Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Salma Salah",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(fontSize: 28, color: Colors.black),
               ),
             ],
           ),
-        
-        
-        const SizedBox(
-          height: 30,
         ),
-
-        Text(
-            "Salma Salah",
-            style: Theme.of(context)
-                .textTheme
-                .headline6!
-                .copyWith(fontSize: 28, color: Colors.black),
-          ),
-        ],
       ),
-      ),
-),
       floatingActionButton: FloatingActionButton(
-        onPressed:()=> Navigator.of(context).pushNamed('/editprofile'),
+        backgroundColor: Colors.cyan,
+        onPressed: () => Navigator.of(context).pushNamed('/editprofile'),
         // tooltip: 'Increment',
-        child: const Icon(Icons.edit),
-      ), 
-    
+        child: const Icon(
+          Icons.edit,
+        ),
+      ),
     );
   }
 }
