fba76b39011b28ae8290fd0f59f0a88d4366b22b
SamaRostom <sama1904157@miuegypt.edu.eg>
2023-03-01 20:16:30 +0200

    Update history.dart

--- rejuvenate_mobile_app/lib/screens/dashboard/history.dart
+++ rejuvenate_mobile_app/lib/screens/dashboard/history.dart
@@ -22,121 +22,105 @@ class History extends StatelessWidget {
                 flex: 10,
                 child: SafeArea(
                   child: SingleChildScrollView(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 30, horizontal: 30),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         // Table
                         DataTable(
-                          // ignore: prefer_const_literals_to_create_immutables
-                          columns: [
-                            const DataColumn(
+                          columnSpacing: 30,
+                          columns: const [
+                            DataColumn(
                                 label: Text('Name',
                                     style: TextStyle(
                                         fontSize: 18,
                                         color: Colors.cyan,
                                         fontWeight: FontWeight.bold))),
-                            const DataColumn(
+                            DataColumn(
                                 label: Text('Code',
                                     style: TextStyle(
                                         fontSize: 18,
                                         color: Colors.cyan,
                                         fontWeight: FontWeight.bold))),
-                            const DataColumn(
+                            DataColumn(
                                 label: Text('Password',
                                     style: TextStyle(
                                         fontSize: 18,
                                         color: Colors.cyan,
                                         fontWeight: FontWeight.bold))),
-                            const DataColumn(
+                            DataColumn(
                                 label: Text('View',
                                     style: TextStyle(
                                         fontSize: 18,
                                         color: Colors.cyan,
                                         fontWeight: FontWeight.bold))),
                           ],
-                          // ignore: prefer_const_literals_to_create_immutables
-                          rows: [
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                          rows: const [
+                            DataRow(cells: [
                               DataCell(Text('1')),
                               DataCell(Text('Stephen')),
                               DataCell(Text('Actor')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('5')),
                               DataCell(Text('John')),
                               DataCell(Text('Student')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('10')),
                               DataCell(Text('Harry')),
                               DataCell(Text('Leader')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
                               DataCell(Text('Actor')),
                             ]),
-                            // ignore: prefer_const_literals_to_create_immutables
-                            const DataRow(cells: [
+                            DataRow(cells: [
                               DataCell(Text('15')),
                               DataCell(Text('Peter')),
                               DataCell(Text('Scientist')),
