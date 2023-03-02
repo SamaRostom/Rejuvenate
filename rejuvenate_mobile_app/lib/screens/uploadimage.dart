d7e238f40a140a8f5fc8afd7ef1dd12b5588eeac
YaraLotfy <96872220+YaraLotfy@users.noreply.github.com>
2023-03-02 12:44:02 +0200

    Update uploadimage.dart

--- rejuvenate_mobile_app/lib/screens/uploadimage.dart
+++ rejuvenate_mobile_app/lib/screens/uploadimage.dart
@@ -1,7 +1,7 @@
 import 'dart:ffi';
 import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../widgets/custom_text.dart';
@@ -18,64 +18,72 @@ class _UploadImageState extends State<UploadImage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CustomText(
-              shadows: [
-                Shadow(
-                  blurRadius: 30,
-                  color: Colors.cyan,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 10,
+                    color: Colors.cyan,
+                  ),
+                ],
+                text: 'Choose which way to enter the image ',
+                fontSize: 30,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.cyan),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(10)),
+                    textStyle: MaterialStateProperty.all(
+                        const TextStyle(fontSize: 14, color: Colors.white))),
+                onPressed: () async {
+                  XFile? xFile = await ImagePicker()
+                      .pickImage(source: ImageSource.gallery);
+                  myfile = File(xFile!.path);
+                  setState(() {});
+                },
+                child: Column(
+                  children: const [
+                    Icon(Icons.image_outlined),
+                    Text(
+                      'Pick Gallery',
+                      style: TextStyle(),
+                    ),
+                  ],
                 ),
-              ],
-              text: ' Choose How To Enter The Image ',
-              fontSize: 40,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  padding: MaterialStateProperty.all(const EdgeInsets.all(10)),
-                  textStyle: MaterialStateProperty.all(
-                      const TextStyle(fontSize: 14, color: Colors.white))),
-              onPressed: () async {
-                XFile? xFile =
-                    await ImagePicker().pickImage(source: ImageSource.gallery);
-                myfile = File(xFile!.path);
-                setState(() {});
-              },
-              child: Column(
-                children: const [
-                  Icon(Icons.image_outlined),
-                  Text(
-                    'Pick Gallery',
-                    style: TextStyle(),
-                  ),
-                ],
               ),
-            ),
-            const SizedBox(height: 10.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
+              const SizedBox(height: 10.0),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.cyan),
+                    padding:
+                        MaterialStateProperty.all(const EdgeInsets.all(10)),
+                    textStyle: MaterialStateProperty.all(
+                        const TextStyle(fontSize: 14, color: Colors.white))),
+                onPressed: () async {
+                   Navigator.pushNamed(context, '/uploadimage');
+                  XFile? xFile =
+                      await ImagePicker().pickImage(source: ImageSource.camera);
+                  myfile = File(xFile!.path);
+                },
+                child: Column(
+                  children: const [
+                    Icon(Icons.camera_alt_outlined),
+                    Text(
+                      'Pick Camera',
+                      style: TextStyle(),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () async {
-                XFile? xFile =
-                    await ImagePicker().pickImage(source: ImageSource.camera);
-                myfile = File(xFile!.path);
-              },
-              child: Column(
-                children: const [
-                  Icon(Icons.camera_alt_outlined),
-                  Text(
-                    'Pick Camera',
-                    style: TextStyle(),
-                  ),
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
