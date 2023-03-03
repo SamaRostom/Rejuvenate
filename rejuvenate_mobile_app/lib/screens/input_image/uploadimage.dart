f00dd8bc3b0b89ebe5e87dbe2cd826fa3f44e1ca
YaraLotfy <96872220+YaraLotfy@users.noreply.github.com>
2023-03-04 00:54:58 +0200

    Update uploadimage.dart

--- rejuvenate_mobile_app/lib/screens/input_image/uploadimage.dart
+++ rejuvenate_mobile_app/lib/screens/input_image/uploadimage.dart
@@ -24,16 +24,9 @@ class _UploadImageState extends State<UploadImage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.cyan,
-        title: Center(
+        title: const Center(
           child: Padding(
-            padding: const EdgeInsets.only(right: 50.0),
-            child: Text(
-              "Upload Image",
-              style: Theme.of(context)
-                  .textTheme
-                  .headline6!
-                  .copyWith(fontSize: 28, color: Colors.white),
-            ),
+            padding: EdgeInsets.only(right: 50.0),
           ),
         ),
       ),
@@ -43,70 +36,77 @@ class _UploadImageState extends State<UploadImage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const CustomText(
-                shadows: [
-                  Shadow(
-                    blurRadius: 10,
-                    color: Colors.cyan,
-                  ),
-                ],
-                text: 'Choose which way to enter the image ',
-                fontSize: 30,
+              Text(
+                "Upload Image",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(fontSize: 30, color: Colors.cyan),
+                textAlign: TextAlign.center,
               ),
               const SizedBox(height: 20),
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.cyan),
-                    padding:
-                        MaterialStateProperty.all(const EdgeInsets.all(10)),
-                    textStyle: MaterialStateProperty.all(
-                        const TextStyle(fontSize: 14, color: Colors.white))),
-                onPressed: () async {
-                  XFile? xFile = await ImagePicker()
-                      .pickImage(source: ImageSource.gallery);
-                  myfile = File(xFile!.path);
-                   Navigator.push(context, MaterialPageRoute(
-                   builder: (context) => PreviewPage(
-                         myfile: xFile,
-                       )));
-                },
-                child: Column(
-                  children: const [
-                    Icon(Icons.image_outlined),
-                    Text(
-                      'Pick Gallery',
-                      style: TextStyle(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.cyan),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.fromLTRB(40, 10, 40, 10)),
+                        textStyle: MaterialStateProperty.all(const TextStyle(
+                            fontSize: 14, color: Colors.white))),
+                    onPressed: () async {
+                      XFile? xFile = await ImagePicker()
+                          .pickImage(source: ImageSource.gallery);
+                      myfile = File(xFile!.path);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PreviewPage(
+                                    myfile: xFile,
+                                  )));
+                    },
+                    child: Column(
+                      children: const [
+                        Icon(Icons.image_outlined),
+                        Text(
+                          'Pick Gallery',
+                          style: TextStyle(),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.cyan),
-                    padding:
-                        MaterialStateProperty.all(const EdgeInsets.all(10)),
-                    textStyle: MaterialStateProperty.all(
-                        const TextStyle(fontSize: 14, color: Colors.white))),
-                onPressed: () async {
-                  XFile? xFile =
-                      await ImagePicker().pickImage(source: ImageSource.camera);
-                   myfile = File(xFile!.path);
-                  
-                   Navigator.push(context, MaterialPageRoute(
-                   builder: (context) => PreviewPage(
-                         myfile: xFile,
-                       )));
-                },
-                child: Column(
-                  children: const [
-                    Icon(Icons.camera_alt_outlined),
-                    Text(
-                      'Pick Camera',
-                      style: TextStyle(),
+                  ),
+                  const SizedBox(width: 10.0),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(Colors.cyan),
+                        padding: MaterialStateProperty.all(
+                            const EdgeInsets.fromLTRB(40, 10, 40, 10)),
+                        textStyle: MaterialStateProperty.all(const TextStyle(
+                            fontSize: 14, color: Colors.white))),
+                    onPressed: () async {
+                      XFile? xFile = await ImagePicker()
+                          .pickImage(source: ImageSource.camera);
+                      myfile = File(xFile!.path);
+
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PreviewPage(
+                                    myfile: xFile,
+                                  )));
+                    },
+                    child: Column(
+                      children: const [
+                        Icon(Icons.camera_alt_outlined),
+                        Text(
+                          'Pick Camera',
+                          style: TextStyle(),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
