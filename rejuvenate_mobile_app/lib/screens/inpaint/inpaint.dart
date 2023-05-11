f64d8534da79da90d6d13ff2aece0a729bfe7339
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2023-05-11 15:35:14 +0300

    Update inpaint.dart

--- rejuvenate_mobile_app/lib/screens/inpaint/inpaint.dart
+++ rejuvenate_mobile_app/lib/screens/inpaint/inpaint.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 import 'package:image_picker/image_picker.dart';
@@ -35,13 +35,11 @@ class _InpaintPageState extends ConsumerState<InpaintPage> {
   Future<void> _getMask(ImageSource source) async {
     final pickedFile = await ImagePicker().getImage(source: source);
     setState(() {
-      // _mask = File('C:\\x\\htdocs\\Graduation\\Rejuvenate\\rejuvenate_mobile_app\\assets\\31.jpg');
       _mask = File(pickedFile!.path);
     });
   }
 
   Future<void> _inpaintImage() async {
-    // _mask = File('C:\\x\\htdocs\\Graduation\\Rejuvenate\\rejuvenate_mobile_app\\assets\\31.jpg');
     if (_image == null || _mask == null) {
       return;
     }
@@ -55,13 +53,8 @@ class _InpaintPageState extends ConsumerState<InpaintPage> {
       _image!.path,
       contentType: MediaType('image', 'jpeg'),
     ));
-    // request.files.add(await http.MultipartFile.fromPath('mask','D:/Graduation project/Mask generation/Steamsamayurkey/masked2/31.jpg',
-    //     contentType: MediaType('image', 'jpeg')));
     request.files.add(await http.MultipartFile.fromPath('mask', _mask!.path,
-        contentType: MediaType('image', 'jpeg')));  
-
-    // Send the request and wait for the response
-    // final response = await http.Response.fromStream(await request.send());
+        contentType: MediaType('image', 'jpeg')));
 
     final response = await request.send();
     if (response.statusCode == 200) {
@@ -76,76 +69,93 @@ class _InpaintPageState extends ConsumerState<InpaintPage> {
       });
       if (imageData != null) {
         Navigator.pushNamed(context, '/finalresult');
-       }
-       else{
+      } else {
         print("no image errorrrrrr");
-       }
+      }
     } else {
-      print("Error"+ response.statusCode.toString());
+      print("Error" + response.statusCode.toString());
     }
-
-    // Decode the response into an image and update the state
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          foregroundColor: const Color.fromARGB(255, 1, 6, 29),
-          shadowColor: Colors.white,
-        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        foregroundColor: const Color.fromARGB(255, 1, 6, 29),
+        shadowColor: Colors.white,
+      ),
       body: Column(
         children: [
+          const SizedBox(height: 30),
+          Image.asset(
+            'assets/upload.jpg',
+            height: 100,
+            width: 100,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 5, left: 50),
+            child: Text(
+              "Upload the image you want \n\t\t\t\t\t to see the result for",
+              style: GoogleFonts.nunitoSans(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  color: const Color.fromARGB(255, 1, 6, 29)),
+            ),
+          ),
           Expanded(
             child: SingleChildScrollView(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  _image != null?Image.file(_image!):
-                  ElevatedButton(
-                     style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.blue[900]),
-                        padding: MaterialStateProperty.all(
-                            const EdgeInsets.fromLTRB(50, 20, 70, 20)),
-                        textStyle: MaterialStateProperty.all(const TextStyle(
-                            fontSize: 20, color: Colors.white))),
-                    onPressed: () => _getImage(ImageSource.gallery),
-                    child: const Text('Select Image'),
-                  ),
-                  _mask != null?Image.file(_mask!):
-                  ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(Colors.blue[900]),
-                        padding: MaterialStateProperty.all(
-                            const EdgeInsets.fromLTRB(50, 20, 70, 20)),
-                        textStyle: MaterialStateProperty.all(const TextStyle(
-                            fontSize: 20, color: Colors.white))),
-                    onPressed: () => _getMask(ImageSource.gallery),
-                    child: const Text('Select Mask'),
+                  const SizedBox(height: 100),
+                  _image != null
+                      ? Image.file(_image!)
+                      : Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue[900],
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 70, vertical: 20),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50))),
+                            onPressed: () => _getImage(ImageSource.gallery),
+                            child: const Text('Select Image',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20)),
+                          ),
+                        ),
+                  const SizedBox(height: 150),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 70, vertical: 20),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                          )),
+                      onPressed: _inpaintImage,
+                      child: const Text(
+                        'Final Results',
+                        style: TextStyle(
+                            color: Color.fromRGBO(13, 71, 161, 1),
+                            fontSize: 20),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
           ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue[900],
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 70, vertical: 20),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(40))),
-            onPressed: _inpaintImage,
-             child: const Text(
-                  'Final Results',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                ),
-          ),
-          // if (imageData != null) Expanded(child: Image.memory(ref.watch(outputimageProvider.notifier).state!) )
-          // else const CircularProgressIndicator(),
+          if (imageData != null)
+            Expanded(
+                child: Image.memory(
+                    ref.watch(outputimageProvider.notifier).state!))
+          else
+            const CircularProgressIndicator(),
         ],
       ),
     );
