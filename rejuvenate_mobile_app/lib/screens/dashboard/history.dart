70ae88ba280eb4fde0e56ad2967b7b1520797504
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2023-05-12 01:47:18 +0300

    Update history.dart

--- rejuvenate_mobile_app/lib/screens/dashboard/history.dart
+++ rejuvenate_mobile_app/lib/screens/dashboard/history.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rejuvenate_mobile_app/services/history_service.dart';
 
 import '../../config/size_config.dart';
@@ -21,66 +22,69 @@ class _HistoryState extends ConsumerState<History> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
-                flex: 10,
+                flex: 30,
                 child: SafeArea(
                   child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        FutureBuilder(
-                            future: HistoryService.gethistory(ref
-                                .read(newUserDataProivder.notifier)
-                                .state!
-                                .ID),
-                            builder: ((context, snapshot) {
-                              if (snapshot.hasData) {
-                                final data = snapshot.data!;
-                                // ignore: avoid_print
-                                print(data);
-                                return SizedBox(
-                                  height: 400,
-                                  child: ListView.builder(
-                                    itemCount: data["listofpatients"].length,
-                                    itemBuilder: (context, index) {
-                                      return FutureBuilder(
-                                          future: HistoryService.getpatient(
-                                              data["listofpatients"][index]),
-                                          builder: (context, snapshot) {
-                                            if (snapshot.hasData) {
-                                              final patientdata =
-                                                  snapshot.data!;
-                                              return Padding(
-                                                padding: const EdgeInsets.only(
-                                                    left: 10),
-                                                child: ListTile(
-                                                  leading: const Icon(
-                                                      Icons.person,
-                                                      color: Color.fromARGB(
-                                                          255, 4, 20, 93)),
-                                                  title: Text(
-                                                      patientdata["fname"] +
-                                                          " " +
-                                                          patientdata["lname"]),
-                                                  subtitle: Text(patientdata[
-                                                      "problemtype"]),
-                                                ),
-                                              );
-                                            }
-                                            return Container();
-                                          });
-                                    },
-                                  ),
-                                );
-                              } else if (snapshot.hasError) {
-                                return Text(snapshot.error.toString());
-                              }
-                              return Container();
-                            }))
-                        // Table
-                      ],
-                    ),
+                    child: Column(children: [
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        "Patients",
+                        style: GoogleFonts.nunitoSans(
+                            fontSize: 25,
+                            color: const Color.fromARGB(255, 13, 71, 161),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const Divider(
+                        color: Colors.black12,
+                      ),
+                      FutureBuilder(
+                          future: HistoryService.gethistory(
+                              ref.read(newUserDataProivder.notifier).state!.ID),
+                          builder: ((context, snapshot) {
+                            if (snapshot.hasData) {
+                              final data = snapshot.data!;
+                              print(data);
+                              return SizedBox(
+                                height: 400,
+                                child: ListView.builder(
+                                  itemCount: data["listofpatients"].length,
+                                  itemBuilder: (context, index) {
+                                    return FutureBuilder(
+                                        future: HistoryService.getpatient(
+                                            data["listofpatients"][index]),
+                                        builder: (context, snapshot) {
+                                          if (snapshot.hasData) {
+                                            final patientdata = snapshot.data!;
+                                            return Padding(
+                                              padding: const EdgeInsets.only(
+                                                  left: 10),
+                                              child: ListTile(
+                                                leading: const Icon(
+                                                    Icons.person,
+                                                    color: Color.fromARGB(
+                                                        255, 13, 71, 161)),
+                                                title: Text(
+                                                    patientdata["fname"] +
+                                                        " " +
+                                                        patientdata["lname"]),
+                                                subtitle: Text(
+                                                    patientdata["problemtype"]),
+                                              ),
+                                            );
+                                          }
+                                          return Container();
+                                        });
+                                  },
+                                ),
+                              );
+                            } else if (snapshot.hasError) {
+                              return Text(snapshot.error.toString());
+                            }
+                            return Container();
+                          }))
+                    ]),
                   ),
                 )),
           ],
